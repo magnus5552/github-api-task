da67bc933f8be2e1aa0ae4b83209deec82e26f98
magnus5552 <isackov.stepa@yandex.ru>
2023-11-06 18:29:58 +0500

    Add tqdm, logging and support limitations

--- activity_counter.py
+++ activity_counter.py
@@ -1,50 +1,74 @@
 import asyncio
+import math
+import re
 import sys
 import time
 from collections import Counter
-
-import re
 from itertools import chain
 
-from github_api import get_all_items, GithubClient
+from aiolimiter import AsyncLimiter
+from tqdm.asyncio import tqdm, tqdm_asyncio
+
+import github_api
+from github_api import GithubClient
 
 page_expr = re.compile(r'&page=(\d*)')
 
 
 async def get_commits_emails_by_page(repo_name: str,
                                      page: int,
-                                     client: GithubClient):
-    commits = await client.get_commits(repo_name, page)
+                                     client: GithubClient,
+                                     limiter):
+    async with limiter:
+        commits = await client.get_commits(repo_name, page)
+
     emails = [x['commit']['author']['email'] for x in commits
               if not x['commit']['message'].startswith('Merge')]
     return emails
 
 
 async def get_commits_emails(repo_name: str, client: GithubClient):
-    pages_count = await get_pages_count(f'https://api.github.com/repos/{repo_name}/commits', client)
+    commits_count = await get_elements_count(
+        f'https://api.github.com/repos/{repo_name}/commits', client)
+    pages_count = roundup(commits_count)
 
-    emails_by_pages = [asyncio.ensure_future(get_commits_emails_by_page
-                                             (repo_name, page, client))
-                       for page in range(1, pages_count + 1)]
+    limiter = AsyncLimiter(100, 1)
+
+    emails_by_pages = [asyncio.ensure_future(
+        get_commits_emails_by_page(repo_name, page, client, limiter))
+        for page in range(1, pages_count + 1)]
     emails = await asyncio.gather(*emails_by_pages)
+
     emails = chain.from_iterable(emails)
     return Counter(emails)
 
 
+async def get_repos_by_page(org_name, page, client, progress_bar: tqdm):
+    repos = await client.get_org_repos(org_name, page)
+    progress_bar.update(len(repos))
+    return [x['full_name'] for x in repos]
+
+
 async def get_all_repos(org_name: str, client: GithubClient):
-    pages_count = await get_pages_count(
-        f'https://api.github.com/org/{org_name}/repos', client)
+    repos_count = await get_elements_count(
+        f'https://api.github.com/orgs/{org_name}/repos', client)
+    pages_count = roundup(repos_count)
 
-    repos_by_page = \
-        [asyncio.ensure_future(client.get_org_repos(org_name, page))
-         for page in range(1, pages_count + 1)]
-    repos_by_page = await asyncio.gather(*repos_by_page)
-    return [x['full_name'] for repos in repos_by_page for x in repos]
+    with tqdm(desc='fetching repositories',
+              total=repos_count,
+              leave=False) as progress_bar:
+        repos_by_page = []
+        for page in range(1, pages_count + 1):
+            repos_by_page.append(asyncio.ensure_future(
+                get_repos_by_page(org_name, page, client, progress_bar)))
+
+        repos_by_page = await asyncio.gather(*repos_by_page)
+    return chain.from_iterable(repos_by_page)
 
 
-async def get_pages_count(url: str, client: GithubClient):
+async def get_elements_count(url: str, client: GithubClient):
     response = await client.session.get(url,
-                                        params={'per_page': 100},
+                                        params={'per_page': 1},
                                         timeout=20)
     if 'last' not in response.links:
         return 1
@@ -53,18 +77,22 @@ async def get_pages_count(url: str, client: GithubClient):
     return int(page)
 
 
-async def main():
-    start = time.time()
+def roundup(x):
+    return int(math.ceil(x / 100.0))
 
-    org_name = sys.argv[1]
+
+async def main(org_name: str):
+    start = time.time()
 
     authors_count = Counter()
     async with GithubClient() as client:
         repos = await get_all_repos(org_name, client)
-
         commit_stats = [asyncio.ensure_future(get_commits_emails(repo, client))
                         for repo in repos]
-        commit_stats = await asyncio.gather(*commit_stats)
+        commit_stats = await tqdm_asyncio.gather(*commit_stats,
+                                                 desc='processing repositories',
+                                                 total=len(commit_stats),
+                                                 leave=False)
 
     for stat in commit_stats:
         authors_count.update(stat)
@@ -77,4 +105,6 @@ async def main():
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    organization_name = sys.argv[1]
+    github_api.LOGGING_ENABLED = len(sys.argv) > 2 and sys.argv[2] == '--log'
+    asyncio.run(main(organization_name))
--- github_api.py
+++ github_api.py
@@ -1,21 +1,14 @@
+import asyncio
+import functools
 import os.path
+import sys
+import time
 
 import httpx
 
 API_ENDPOINT = 'https://api.github.com'
 HEADERS = {'Accept': 'application/vnd.github+json'}
-
-
-async def get_all_items(func):
-    returned_count = 100
-    page = 1
-    while returned_count == 100:
-        items = await func(page)
-        for item in items:
-            yield item
-
-        returned_count = len(items)
-        page += 1
+LOGGING_ENABLED = False
 
 
 def configure_session():
@@ -27,30 +20,58 @@ def configure_session():
     return session
 
 
+def log(func):
+    @functools.wraps(func)
+    async def wrapper(self, *args, **kwargs):
+        if not LOGGING_ENABLED:
+            return await func(self, *args, **kwargs)
+
+        start = time.time()
+        try:
+            result = await func(self, *args, **kwargs)
+            print('success', '-', time.time() - start, ':', func.__name__, *args, **kwargs)
+            return result
+        except httpx.HTTPStatusError as e:
+            print('fail', func.__name__, *args, **kwargs, file=sys.stderr)
+            print(e.response.text, file=sys.stderr)
+            raise
+
+    return wrapper
+
+
 class GithubClient:
 
     async def get(self, request: str, params=None, headers=None):
-        try:
-            print('PENDING -', request, params)
+        await self.event.wait()
+        async with self.semaphore:
             response = await self.session.get(API_ENDPOINT + request,
                                               headers=headers,
-                                              params=params,
-                                              timeout=20)
-            response.raise_for_status()
-            return response.json()
-        except:
-            print('FAILED -', request, params)
-            raise
+                                              params=params)
+            if 'retry-after' in response.headers:
+                wait_time = int(response.headers['retry-after'])
+                self.event.clear()
+                await asyncio.sleep(wait_time)
+                self.event.set()
+                response = await self.session.get(API_ENDPOINT + request,
+                                                  headers=headers,
+                                                  params=params)
+        response.raise_for_status()
+        return response.json()
 
+    @log
     async def get_org_repos(self, org_name: str, page: int):
         params = {'page': page, 'per_page': 100}
         return await self.get(f'/orgs/{org_name}/repos', params=params)
 
+    @log
     async def get_commits(self, repo_name: str, page: int):
         params = {'page': page, 'per_page': 100}
         return await self.get(f'/repos/{repo_name}/commits', params=params)
 
     async def __aenter__(self):
+        self.semaphore = asyncio.Semaphore(100)
+        self.event = asyncio.Event()
+        self.event.set()
         self.session = configure_session()
         return self
 
